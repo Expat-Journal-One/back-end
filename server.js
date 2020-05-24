76778af7bdabbcdda3939981e8beeab135f7577e
Christina Harris <christinathedesigner@outlook.com>
2020-05-24 11:51:05 -0700

    testing automated documentation

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-const docs = require("express-docs");
+var docs = require("express-docs");
 server.use("/api/users", userRouter);
 server.use("/api/stories", storyRouter);
 server.use("/api/auth", authRouter)
